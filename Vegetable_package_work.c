e3b01cd61e4c825a7053c99b692b9c9942c004a9
muzhiyun <my616422@126.com>
2018-06-19 03:05:49 +0800

    Update Vegetable_package_work.c

--- Vegetable_package_work.c
+++ Vegetable_package_work.c
@@ -1,11 +1,12 @@
 //朋友选修课的作业 好吧 我知道这很逊   qwq~
 //貌似可以用了   
+//第三次修改  纯手工打造  没学过数据结构和算法  c语言看到指针就学不下去了  死磕出真理
 
 #include<stdio.h>
 #include <stdlib.h>   //提供清屏支持
 #define N 5   //宏定义  定义学生总数
 
-void input(float x[N][6],int numtmp[N])     //录入函数 录入四科成绩同时初始化学号数组   同一个学生的四科成绩储存在[0][0] --[0][3]
+void input(float x[N][6],int numtmp[N])     //录入函数 录入四科成绩同时初始化学号数组   
 {
 	int i,j;
 	float sum=0;
@@ -14,7 +15,7 @@ void input(float x[N][6],int numtmp[N])     //录入函数 录入四科成绩同
 	{
 		numtmp[j]=j+1;
 		printf("请输入学号为%d学生的成绩\n",numtmp[j]);
-		for (i=0;i<4;i++)
+		for (i=0;i<4;i++)                  //同一个学生的四科成绩储存在[0][0] --[0][3]
 		{
 		scanf("%f",&x[j][i]);
 		}
@@ -109,27 +110,21 @@ void High (float x[][6],char strtmp[N][20])  //得出最高分并打印高分者
 		for (tmp=x[0][i],num=0,j=0;j<N;j++)
 		{
 			if (tmp<x[j][i]) {tmp=x[j][i];num=j;}
-			//if (i==2) {printf("      %.2f",x[j][i]);}
 		}
 	printf("     %s",strtmp[num]);
-	//printf("      %.2f",tmp);
 	}
 	printf("\n");
 }	
  
-int main()
-{   int tmp,numb[N];                                                       //学号数组
-	char str[N][20]={"李小琳","张飞瀛","宁  可","王明云","朱鸿儒"};   //字符串数组  储存姓名  
-	float a[N][6];  	//成绩表数组
-	input(a,numb);
-	Sort(a,str,numb);
-	while (1)    //菜单函数 本来打算写成子函数 但调用其他子函数存在问题  放弃
+void menu (float x[N][6],char strtmp[N][20],int numtmp[N]) //菜单函数 调用其他子函数
+{
+	int tmp;
+	while (1)    
 		{
 		
 			printf("\n\n-------------操作选项-----------\n");
 			printf("1:打印成绩不显示最高分最低分者 \n");
 			printf("2:打印成绩和各科最高分最低分者 \n");
-			//printf("3:退出程序 \n");
 			printf("--------------------------------\n");
 			printf("按数字键选择要执行的操作: ");
 			scanf("%d",&tmp);
@@ -137,13 +132,22 @@ int main()
 			system("cls");
 			switch(tmp)
 			{
-				case 1:  output(a,str,numb);break;    
-				case 2:  output(a,str,numb);Low(a,str);High(a,str);break;     
+				case 1:  output(x,strtmp,numtmp);break;    
+				case 2:  output(x,strtmp,numtmp);Low(x,strtmp);High(x,strtmp);break;     
 				default:          //数字输入错误，跳出siwtch语句，进入下一次循环
 					printf("输入的数字不正确\n");
 				break;
 				}
 		}
+	}
+
+int main()
+{   int numb[N];                                                       //定义学号数组
+	char str[N][20]={"李小琳","张飞瀛","宁  可","王明云","朱鸿儒"};   //定义字符串数组  储存姓名  
+	float a[N][6];  	   //定义成绩表数组
+	input(a,numb);         //开始录入并求出总分和平均分
+	Sort(a,str,numb);      //开始按总分排序
+	menu(a,str,numb);      //显示菜单
 	}
 
 	
