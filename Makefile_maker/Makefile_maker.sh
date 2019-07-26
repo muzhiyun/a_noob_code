56ea08aeddb45d0b627e8e0e6054f5f46b4f372d
muzhiyun <my616422@126.com>
2019-07-26 21:20:19 +0800

    modify Makefile_make.sh

--- Makefile_maker/Makefile_maker.sh
+++ Makefile_maker/Makefile_maker.sh
@@ -12,12 +12,17 @@ echo 'prom = '$1 >> Makefile
 echo 'deps = $(shell find ./ -maxdepth 1 -name '"'"'*.h'"'"'|sed '"'"'s%.*/%%'"'"')' >> Makefile
 echo 'src = $(shell find ./ -maxdepth 1 -name '"'"'*.c'"'"'|sed '"'"'s%.*/%%'"'"')' >> Makefile
 echo 'obj = $(src:%.c=%.o)' >> Makefile
+echo 'lib = '$2 $3 $4 >> Makefile
 echo '' >> Makefile
 echo '$(prom):$(obj)' >> Makefile
-echo '	$(cc) -o $(prom) $(obj)' >> Makefile
+
+echo '	$(cc) -o $(prom) $(obj) $(lib)' >> Makefile
 echo '' >> Makefile
 echo '%.o:%.c $(deps)' >> Makefile
-echo '	$(cc) -c $< -o $@' >> Makefile
+echo '	$(cc) -c $< -o $@ $(lib)' >> Makefile
+
+
+
 echo '' >> Makefile
 echo 'clean:' >> Makefile
 echo '	rm -rf $(obj) $(prom)' >> Makefile
