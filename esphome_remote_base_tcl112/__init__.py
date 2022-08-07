48ed01edda56ba6b297220846699eb7728dd664c
muzhiyun <my616422@126.com>
2022-08-07 18:46:37 +0800

    Fix remote_base build error due to diff esphome version

--- esphome_remote_base_tcl112/__init__.py
+++ esphome_remote_base_tcl112/__init__.py
@@ -32,7 +32,7 @@ from esphome.const import (
     CONF_LEVEL,
 )
 from esphome.core import coroutine
-from esphome.schema_extractors import SCHEMA_EXTRACT, schema_extractor
+from esphome.jsonschema import jschema_extractor
 from esphome.util import Registry, SimpleRegistry
 
 AUTO_LOAD = ["binary_sensor"]
@@ -195,14 +195,14 @@ def validate_dumpers(value):
 def validate_triggers(base_schema):
     assert isinstance(base_schema, cv.Schema)
 
-    @schema_extractor("triggers")
+    @jschema_extractor("triggers")
     def validator(config):
         added_keys = {}
         for key, (_, valid) in TRIGGER_REGISTRY.items():
             added_keys[cv.Optional(key)] = valid
         new_schema = base_schema.extend(added_keys)
-
-        if config == SCHEMA_EXTRACT:
+        # pylint: disable=comparison-with-callable
+        if config == jschema_extractor:
             return new_schema
         return new_schema(config)
 
